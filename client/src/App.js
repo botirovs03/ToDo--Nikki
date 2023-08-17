b99652837d13e97c60d3b6baf6291c8aeec572d9
jdu211171 <211171m@jdu.uz>
2023-08-18 02:22:22 +0500

    Login page with errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,13 @@
 // client/src/App.js
 
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Login from "./Login";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+    <Login />
+  )
 }
 
 export default App;
