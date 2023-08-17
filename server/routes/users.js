4ae905eea3bf12cd480a17e89288baf168de83d3
jdu200027 <200027a@jdu.uz>
2023-08-17 13:41:25 +0500

    "User add, login, put, delete"

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,100 +1,193 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const authenticateUser = require("../middleware/authenticateUser");
-const connection = require("../config/db"); // Your updated database configuration with mysql2
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const authenticateUser = require('../middleware/authenticateUser');
+const connection = require('../config/db');
 
-const SECRET_KEY = "your_secret_key_here";
+const SECRET_KEY = 'your_secret_key';
+
 const router = express.Router();
 
-router.post("/api/users", async (req, res) => {
-  try {
-    const { username, password, email } = req.body;
+router.post('/api/users', async (req, res) => {
+    try {
+        const { username, password, email } = req.body;
 
-    // Generate a salt and hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Insert user into the database
-    const insertUserQuery =
-      "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-    connection.query(
-      insertUserQuery,
-      [username, hashedPassword, email],
-      (err, results) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({ error: "Failed to register user" });
-        } else {
-          // User registration successful
-          res.status(201).json({ message: "User registered successfully" });
-        }
-      }
-    );
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+        const inserUserQuery = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+
+        connection.query(
+            inserUserQuery, [username, hashedPassword, email], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error: 'Failed to register user' });
+                } else {
+                    res.status(201).json({ message: 'User registered successfully' });
+                }
+            }
+        );
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}); // Add User
+
+
+router.post('/api/auth', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Find the user in the database
+        connection.query('SELECT * FROM users WHERE username = ?', [username], async (error, results) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ message: 'Internal server error' });
+            };
+
+            if (results.length === 0) {
+                return res.status.json({ message: 'Invalid credentials' });
+            };
+
+            const user = results[0];
+
+
+            // Compare the provided password with the hashed password
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (!passwordMatch) {
+                res.status(401).json({ message: 'Password is invalid' });
+            }
+            const token = jwt.sign({ userId: user.user_id }, SECRET_KEY, {
+                expiresIn: '1h'
+            });
+
+            res.status(200).json({token});
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}); // Check user from sql
+
+
+router.get('/accessResoure', authenticateUser, (req, res) => {
+    try {
+        const decodedToken = jwt.verify(req.headers.authorization.split(' ')[1], SECRET_KEY);
+        res.status(200).json({ success: true, data: { userId: decodedToken.userId, email: decodedToken.email } });
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
 });
 
-
-router.post("/api/auth", async (req, res) => {
-  try {
+router.put('/api/users/:user_id', authenticateUser, async (req, res) => {
+    const userIdToUpdate = req.params.user_id;
     const { username, password } = req.body;
-    // console.log(req.body)
 
-    // Fetch user from the database based on the username
-    const selectUserQuery = "SELECT * FROM users WHERE Username = ?";
-    connection.query(selectUserQuery, [username], async (err, results) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Internal server error" });
-      }
+    try {
+        // Fetch the user's information from the database
+        const fetchUserQuery = 'SELECT * FROM users WHERE user_id = ?';
+        connection.query(fetchUserQuery, [userIdToUpdate], async (error, results) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
 
-      if (results.length === 0) {
-        return res.status(401).json({ error: "Invalid credentials" });
-      }
+            if (results.length === 0) {
+                return res.status(404).json({ message: 'User not found' });
+            }
 
-      const user = results[0];
-      console.log(user.Password)
-      // Compare the provided password with the hashed password in the database
-      const passwordMatch = await bcrypt.compare(password, user.Password);
+            const user = results[0];
 
-      if (!passwordMatch) {
-        return res.status(401).json({ error: "Password Not Match" });
-      }
+            // Check if the authenticated user is the same as the user being updated
+            if (req.userId !== user.user_id) {
+                return res.status(403).json({ message: 'You do not have permission to update this user.' });
+            }
 
-      // Generate a JWT token
-      const token = jwt.sign({ userId: user.UserID }, SECRET_KEY, {
-        expiresIn: "1h", // Set the token expiration time as needed
-      });
+            // Hash the new password if provided
+            const updatedPassword = password ? await bcrypt.hash(password, 10) : user.password;
 
-      // Send the token to the client
-      res.status(200).json({ token });
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+            // Update user information in the database
+            const updateUserQuery = 'UPDATE users SET username = ?, password = ? WHERE user_id = ?';
+            connection.query(updateUserQuery, [username, updatedPassword, userIdToUpdate], (error, results) => {
+                if (error) {
+                    console.error('Error updating user:', error);
+                    return res.status(500).json({ message: 'Internal server error' });
+                }
+                
+                res.status(200).json({ message: 'User updated successfully' });
+            });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
-router.put("/api/users/:userID", authenticateUser, async (req, res) => {
-  // Update user information endpoint code
-  // Modify this section to update user information in the database
+router.delete('/api/users/:user_id', authenticateUser, async (req, res) => {
+    const userIdToDelete = req.params.user_id;
+
+    try {
+        // Fetch the user's information from the database
+        const fetchUserQuery = 'SELECT * FROM users WHERE user_id = ?';
+        connection.query(fetchUserQuery, [userIdToDelete], (error, results) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
+
+            if (results.length === 0) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+
+            const user = results[0];
+
+            // Check if the authenticated user is authorized to delete this user
+            if (req.userId !== user.user_id) {
+                return res.status(403).json({ message: 'Not authorized to delete this user' });
+            }
+
+            // Proceed with user deletion
+            const deleteUserQuery = 'DELETE FROM users WHERE user_id = ?';
+            connection.query(deleteUserQuery, [userIdToDelete], (error, results) => {
+                if (error) {
+                    console.error(error);
+                    return res.status(500).json({ message: 'Internal server error' });
+                }
+
+                res.status(200).json({ message: 'User deleted successfully' });
+            });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
-router.delete("/api/users/:userID", authenticateUser, async (req, res) => {
-  // Delete user endpoint code
-  // Modify this section to delete a user from the database
+
+router.post('/api/users/createCategory/:user_id/:category_name', authenticateUser,  (req, res) => {
+    const userId = req.params.user_id;
+    const categoryName = req.params.category_name;
+
+    try {
+        // Check if the authenticated user is the same as the user creating the category
+        if (req.userId !== userId) {
+            return res.status(403).json({ message: 'Not authorized to create category' });
+        }
+
+        // Insert the new category into the database
+        const insertCategoryQuery = 'INSERT INTO Categories (UserID, CategoryName) VALUES (?, ?)';
+        connection.query(insertCategoryQuery, [userId, categoryName], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
+            res.status(201).json({ message: 'Category created successfully' });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
-router.get("/api/check", (req, res) => {
-  res.json({ message: "Hello from server!2" });
-});
-
-router.get('/accessResource', authenticateUser, (req, res)=>{  
-  
-  res.status(200).json({success:true, data:{userId:req.userId, email:req.email}});   
-})
 
 module.exports = router;
