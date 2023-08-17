974d9188cc6af762e2585af93da5caa4477437b8
jdu211171 <211171m@jdu.uz>
2023-08-18 02:58:49 +0500

    Register page with full of error

---
+++ client/src/Register.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.css';
+
+function Register() {
+  return (
+    <div className="container">
+      <div className="logo"></div>
+
+      <div className="form">
+        <span className="title">Create your account</span>
+        <span>organize your work</span>
+        <form action="" method="">
+          <label htmlFor="fname">first name</label>
+          <input name="firstname" type="text" placeholder="Write your first name"/>
+
+          <label htmlFor="lname">last name</label>
+          <input name="lastname" type="text" placeholder="Write your last name"/>
+
+          <label htmlFor="email">email address</label>
+          <input name="email" type="mail" placeholder="Enter your email"/>
+
+          <label htmlFor="password">password</label>
+          <input name="password" type="password" placeholder="Create a secure password"/>
+
+          <label htmlFor="rpassword">repeat password</label>
+          <input name="rpassword" type="password" placeholder="Write password one more time"/>
+
+          <button type="submit">Create account</button>
+        </form>
+        <span>Already have an account?<a href="[1]">Log in</a></span>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
