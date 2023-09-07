eec2f39bc7a6b5dda9607d65f5576c4457959078
Saydiakhror Botirov <t19b0007s@jdu.uz>
2023-09-08 03:45:53 +0500

    set to JapanTimeZone, listed all routes to fix it later

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,84 @@
-// client/src/App.js
-
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  const [data, setData] = React.useState(null);
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Routes>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
--- server/routes/taks.js
+++ server/routes/taks.js
@@ -1,125 +1,165 @@
-const express = require('express');
-const authenticateUser = require('../middleware/authenticateUser');
-const connection = require('../config/db');
+const express = require("express");
+const moment = require("moment-timezone");
+
+const authenticateUser = require("../middleware/authenticateUser");
+const connection = require("../config/db");
 const router = express.Router();
 
-router.post('/api/task', authenticateUser, (req, res) => {
-    const { categoryID, taskName, description, priority, deadline } = req.body;
+// Set the desired time zone (e.g., Japan)
+const japanTimeZone = "Asia/Tokyo";
+moment.tz.setDefault(japanTimeZone);
 
-    // Validate input data
-    if (!categoryID || !taskName || !priority || !deadline) {
-        return res.status(400).json({ error: 'Missing required fields' });
-    }
+router.post("/api/task", authenticateUser, (req, res) => {
+  const { categoryID, taskName, description, priority, deadline } = req.body;
 
-    const userID = req.userId; // Get the authenticated user's ID from req.user
-    console.log("userID is >>>>>>> " + userID);
+  // Validate input data
+  if (!categoryID || !taskName || !priority) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+  // convert to japan time
+  deadlineJap = moment(deadline).format('YYYY-MM-DD HH:mm:ss');;
 
-    // Insert the task into the Tasks table
-    const insertTaskQuery = `
+  const userID = req.userId; // Get the authenticated user's ID from req.user
+  console.log("userID is >>>>>>> " + userID);
+
+  // Insert the task into the Tasks table
+  const insertTaskQuery = `
         INSERT INTO Tasks (UserID, CategoryID, TaskName, Description, Priority, Deadline, Completed)
         VALUES (?, ?, ?, ?, ?, ?, ?)
     `;
 
-    connection.query(
-        insertTaskQuery,
-        [userID, categoryID, taskName, description, priority, deadline, false],
-        (error, results, fields) => {
-            if (error) {
-                console.error(error);
-                return res.status(500).json({ error: 'Error creating task' });
-            }
+  connection.query(
+    insertTaskQuery,
+    [userID, categoryID, taskName, description, priority, deadlineJap, false],
+    (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Error creating task" });
+      }
 
-            return res.status(201).json({ message: 'Task created successfully' });
-        }
-    );
+      return res.status(201).json({ message: "Task created successfully" });
+    }
+  );
 });
 
-router.put('/api/tasks/:taskID', authenticateUser, (req, res) => {
-    const taskID = req.params.taskID;
-    const { taskName, description, priority, deadline, completed, categoryName } = req.body;
+router.put("/api/tasks/:taskID", authenticateUser, (req, res) => {
+  const taskID = req.params.taskID;
+  const { taskName, description, priority, deadline, completed, categoryName } =
+    req.body;
 
-    // Validate input data
-    if (!taskName || !priority ) {
-        return res.status(400).json({ error: 'Missing required fields' });
-    }
+  // Validate input data
+  if (!taskName || !priority) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
 
-    // Update the task in the database
-    const updateTaskQuery = `
+  deadlineJap = moment(deadline).format('YYYY-MM-DD HH:mm:ss');;
+
+  // Update the task in the database
+  const updateTaskQuery = `
         UPDATE tasks
         SET taskName = ?, description = ?, priority = ?, deadline = ?, completed = ?, CategoryID = ?
         WHERE taskID = ?
     `;
 
-    const values = [taskName, description, priority, deadline, completed, categoryName, taskID];
+  const values = [
+    taskName,
+    description,
+    priority,
+    deadlineJap,
+    completed,
+    categoryName,
+    taskID,
+  ];
 
-    connection.query(updateTaskQuery, values, (err, result) => {
-        if (err) {
-            console.error('Error updating task:', err);
-            return res.status(500).json({ error: 'An error occurred while updating the task' });
-        }
+  connection.query(updateTaskQuery, values, (err, result) => {
+    if (err) {
+      console.error("Error updating task:", err);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while updating the task" });
+    }
 
-        return res.status(200).json({ message: 'Task updated successfully' });
-    });
+    return res.status(200).json({ message: "Task updated successfully" });
+  });
 });
 
-router.delete('/api/tasks/:taskID', authenticateUser, (req, res) => {
-    const taskID = req.params.taskID;
+router.delete("/api/tasks/:taskID", authenticateUser, (req, res) => {
+  const taskID = req.params.taskID;
 
-    // Delete the task from the database
-    const deleteTaskQuery = `
+  // Delete the task from the database
+  const deleteTaskQuery = `
         DELETE FROM tasks
         WHERE taskID = ?
     `;
 
-    connection.query(deleteTaskQuery, [taskID], (err, result) => {
-        if (err) {
-            console.error('Error deleting task:', err);
-            return res.status(500).json({ error: 'An error occurred while deleting the task' });
-        }
+  connection.query(deleteTaskQuery, [taskID], (err, result) => {
+    if (err) {
+      console.error("Error deleting task:", err);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while deleting the task" });
+    }
 
-        return res.status(200).json({ message: 'Task deleted successfully' });
-    });
+    return res.status(200).json({ message: "Task deleted successfully" });
+  });
 });
 
-router.put('/api/tasks/:taskID/complete', authenticateUser, (req, res) => {
-    const taskID = req.params.taskID;
+router.put("/api/tasks/:taskID/complete", authenticateUser, (req, res) => {
+  const taskID = req.params.taskID;
 
-    // Retrieve the current completion status of the task
-    const getCompletionStatusQuery = `
+  // Retrieve the current completion status of the task
+  const getCompletionStatusQuery = `
         SELECT completed
         FROM tasks
         WHERE taskID = ?
     `;
 
-    connection.query(getCompletionStatusQuery, [taskID], (err, rows) => {
-        if (err) {
-            console.error('Error retrieving task completion status:', err);
-            return res.status(500).json({ error: 'An error occurred while retrieving the task completion status' });
-        }
+  connection.query(getCompletionStatusQuery, [taskID], (err, rows) => {
+    if (err) {
+      console.error("Error retrieving task completion status:", err);
+      return res.status(500).json({
+        error: "An error occurred while retrieving the task completion status",
+      });
+    }
 
-        if (rows.length !== 1) {
-            return res.status(404).json({ error: 'Task not found' });
-        }
+    if (rows.length !== 1) {
+      return res.status(404).json({ error: "Task not found" });
+    }
 
-        const currentStatus = rows[0].completed;
-        const newStatus = !currentStatus;
+    const currentStatus = rows[0].completed;
+    const newStatus = !currentStatus;
+    let completedate = moment().format('YYYY-MM-DD HH:mm:ss');;
+    console.log(completedate)
+    if (!newStatus) {
+        completedate = null;
+    } 
 
-        // Update the task with the new completion status
-        const updateCompletionStatusQuery = `
+    // Update the task with the new completion status
+    const updateCompletionStatusQuery = `
             UPDATE tasks
-            SET completed = ?, completedDate = NOW()
+            SET completed = ?, completedDate = ?
             WHERE taskID = ?
         `;
 
-        connection.query(updateCompletionStatusQuery, [newStatus, taskID], (err, result) => {
-            if (err) {
-                console.error('Error toggling task completion status:', err);
-                return res.status(500).json({ error: 'An error occurred while toggling the task completion status' });
-            }
+    connection.query(
+      updateCompletionStatusQuery,
+      [newStatus, completedate, taskID],
+      (err, result) => {
+        if (err) {
+          console.error("Error toggling task completion status:", err);
+          return res.status(500).json({
+            error:
+              "An error occurred while toggling the task completion status",
+          });
+        }
 
-            return res.status(200).json({ message: 'Task completion status toggled', completed: newStatus });
+        return res.status(200).json({
+          message: "Task completion status toggled",
+          completed: newStatus,
         });
-    });
+      }
+    );
+  });
 });
 
 module.exports = router;
