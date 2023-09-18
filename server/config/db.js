5456c093e7f8d39633538864c51d6762db7509c7
Saydiakhror Botirov <botirovs03@gmail.com>
2023-09-18 05:46:10 +0500

    set connection reconnect delay

--- server/config/db.js
+++ server/config/db.js
@@ -11,6 +11,10 @@ const connection = mysql.createConnection({
   password: '3b3s2001Remote##',
   database: 'todo',
   connectTimeout: 2000,
+  reconnect: {
+    maxDelay: 2000, // Maximum delay between reconnection attempts (in milliseconds)
+    initialDelay: 1000, // Initial delay for the first reconnection attempt (in milliseconds)
+  },
 });
 
 
